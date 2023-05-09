6032fef6e99dba401c5b2d8475ea82376c2f66bb
Harsh <valambeharsh1701@gmail.com>
2023-05-10 01:56:36 +0530

    4 done

---
+++ arrays/4.cpp
@@ -0,0 +1,8 @@
+//contains duplicates
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return unique(nums.begin(),nums.end())!=nums.end();
+    }
+};
