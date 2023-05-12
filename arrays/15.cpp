04e7efa877694d405344578316bb8ce1b27300a6
Harsh <valambeharsh1701@gmail.com>
2023-05-12 13:10:14 +0530

    15 done

---
+++ arrays/15.cpp
@@ -0,0 +1,22 @@
+//search in a rotated sortedarray
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int i = 0;
+        int j = nums.size() - 1;
+        
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[i]) {
+                if (nums[mid] < target && nums[j] >= target) i = mid+1;
+                else j = mid-1;
+            } else {
+                if (nums[mid] > target && nums[i] <= target) j = mid-1;
+                else i = mid+1;
+            }
+        }
+        
+        return -1;
+    }
+};
