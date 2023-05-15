1f232dc6ef097b9bfac728f0a65d0669177ad851
Harsh <valambeharsh1701@gmail.com>
2023-05-15 16:24:55 +0530

    2 done

---
+++ strings/2.cpp
@@ -0,0 +1,9 @@
+//anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) { 
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t; 
+    }
+};
